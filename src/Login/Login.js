07466e4f2d19cbda235167be3e34094fdc47b5c8
User-1 <user-1@MacBook-28.local>
2019-04-23 17:22:30 -0600

    setting up the Login component

---
+++ src/Login/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './Login.css';
+import { logicalExpression } from '@babel/types';
+
+class Login extends Component{
+
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Login;
