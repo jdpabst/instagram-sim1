dbff53825be1c69fc46c6a5cfba143c8bd4e8b04
User-1 <user-1@MacBook-28.local>
2019-04-29 15:03:38 -0600

    styling home page, search box

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,10 +4,11 @@ import './Home.css'
 class Home extends Component{
     render(){
         return(
-            <div>
+            <div id='home'>
+
                 <div id='header'>
                     <div id='logo-container'>
-                        <img id='logo'  src="https://saltislandseaplanes.com/wp-content/uploads/2017/12/instagram-png-instagram-icon-1600-600x600.png"/>
+                        <img id='logo'  className='img' src="https://saltislandseaplanes.com/wp-content/uploads/2017/12/instagram-png-instagram-icon-1600-600x600.png"/>
                         <div id='line'></div>
                         <h1>Instagram</h1>
                     </div>
@@ -16,12 +17,16 @@ class Home extends Component{
                     {/* src for the img http://www.stickpng.com/assets/images/585e4ae9cb11b227491c3394.png */}
 
                     <div id='navigations-container'>
-                        <img id='compass' src="https://www.freeiconspng.com/uploads/compass-icon-22.png"/>
-                        <img id='heart' src="http://chittagongit.com/download/348095"/>
-                        <img id='profile' src="http://simpleicon.com/wp-content/uploads/user-1.svg" />
+                        <img id='compass' className='img' src="https://www.freeiconspng.com/uploads/compass-icon-22.png"/>
+                        <img id='heart' className='img' src="http://chittagongit.com/download/348095"/>
+                        <img id='profile' className='img' src="http://simpleicon.com/wp-content/uploads/user-1.svg" />
                     </div>
                     
                 </div>
+
+                {/* <div id='home-feed-wrapper'>
+                
+                </div> */}
             </div>
         )
     }
