27909ba0a89c50fed1f6efff215aa197b08beec1
User-1 <user-1@MacBook-28.local>
2019-04-29 15:31:39 -0600

    finished with the home page header

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,26 +7,31 @@ class Home extends Component{
             <div id='home'>
 
                 <div id='header'>
-                    <div id='logo-container'>
-                        <img id='logo'  className='img' src="https://saltislandseaplanes.com/wp-content/uploads/2017/12/instagram-png-instagram-icon-1600-600x600.png"/>
-                        <div id='line'></div>
-                        <h1>Instagram</h1>
-                    </div>
-                    
-                    <input id='search-box' />
-                    {/* src for the img http://www.stickpng.com/assets/images/585e4ae9cb11b227491c3394.png */}
+                    <div id='flex-wrapper'>
+                        <div id='logo-container'>
+                            <img id='logo'  className='img' src="https://saltislandseaplanes.com/wp-content/uploads/2017/12/instagram-png-instagram-icon-1600-600x600.png"/>
+                            <div id='line'></div>
+                            <h1>Instagram</h1>
+                        </div>
+                        
+                        <div id='search-box-wrapper'>
+                            <img src='https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-512.png' id='search-icon' />
+                            <input id='search-box' placeholder='Search' />
+                        </div>
+                       
 
-                    <div id='navigations-container'>
-                        <img id='compass' className='img' src="https://www.freeiconspng.com/uploads/compass-icon-22.png"/>
-                        <img id='heart' className='img' src="http://chittagongit.com/download/348095"/>
-                        <img id='profile' className='img' src="http://simpleicon.com/wp-content/uploads/user-1.svg" />
+                        <div id='navigations-container'>
+                            <img id='compass' className='img' src="https://www.freeiconspng.com/uploads/compass-icon-22.png"/>
+                            <img id='heart' className='img' src="http://chittagongit.com/download/348095"/>
+                            <img id='profile' className='img' src="http://simpleicon.com/wp-content/uploads/user-1.svg" />
+                        </div>
                     </div>
-                    
                 </div>
 
                 {/* <div id='home-feed-wrapper'>
                 
                 </div> */}
+                {/* will map through the items given to us from the database to fill the feed here. Should just create a new component for each. */}
             </div>
         )
     }
