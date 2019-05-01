f682a2db8c1ebfcae0975b43a311f0359aac8796
User-1 <user-1@MacBook-28.local>
2019-05-01 16:27:04 -0600

    working on adding tables to sql db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const aws = require('aws-sdk')
+const express = require('express')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const config = require('../config')
 
 const app = express();
 
 app.use(express.json());
+aws.config.update(config.awsConfig)
+
+massive(config.massiveConnection)
+  .then(db => {
+    app.set('db', db);
+  })
 
 
 const port = 3000;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ImageUploader from 'react-images-upload';
 import './Home.css'
 
 class Home extends Component{
